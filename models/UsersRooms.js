ed80e0630065f2b90548fdae4da0fefeb8203272
Robert Sorkin <robasorkin@gmail.com>
2017-07-12 12:48:58 +0300

    test

--- models/UsersRooms.js
+++
@@ -1,9 +0,0 @@
-module.exports = usersrooms = function(sequelize, DataTypes) {
-	return sequelize.define('usersrooms', {
-		role: DataTypes.STRING,
-		favorite: {
-			type: DataTypes.BOOLEAN,
-			defaultValue: true
-		}
-	});
-};
